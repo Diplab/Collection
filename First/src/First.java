3e0a886c10d766925d34c0aea1393b95b1930baa
thunder5678 <thunder8520@gmail.com>
2014-10-01 15:03:10 +0800

    Try to use TreeSet

---
+++ First/src/First.java
@@ -0,0 +1,22 @@
+import java.util.TreeSet;
+
+public class First {
+
+	public static void main(String[] args) {
+//		 ArrayList<Integer> apples = new ArrayList<Integer>();
+//		 apples.add(1);
+//		 apples.add(2);
+//		 for(Integer integer : apples){
+//		 System.out.println(integer);
+//		 }
+		TreeSet<Integer> tree = new TreeSet<Integer>();
+			for(int i=1;i<13;i+=2){
+				
+				tree.add(i);
+				
+			}
+			
+			
+	}
+
+}
