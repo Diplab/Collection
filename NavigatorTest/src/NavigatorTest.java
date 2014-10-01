94972915de1b748fd81e5d4a0f26fd4bd8ef9a3b
yingjhu <yingjhu@hotmail.com>
2014-10-01 16:32:44 +0800

    Navigable  Change  Tree

--- NavigatorTest/src/NavigatorTest.java
+++ NavigatorTest/src/NavigatorTest.java
@@ -5,11 +5,11 @@ import java.util.TreeSet;
 public class NavigatorTest {
 
 	public static void main(String[] args) {
-		NavigableSet<Integer> a = new TreeSet<Integer>();
+		TreeSet<Integer> a = new TreeSet<Integer>();
 		for (int i = 1; i < 20; i += 3) {
 			a.add(i);
 		}
-		NavigableSet<Integer> subSet = a.subSet(1, true, 10, true);
+		TreeSet<Integer> subSet = (TreeSet<Integer>) a.subSet(1, true, 10, true);
 		
 		System.out.println(a + "\t" + subSet);
 		
