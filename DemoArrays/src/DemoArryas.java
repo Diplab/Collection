da373849025a7f957f1954cff0698e9ee72865d8
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-01 14:36:12 +0800

    Add Project to demo Arrays utilities

---
+++ DemoArrays/src/DemoArryas.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+
+public class DemoArryas {
+
+	public static void main(String[] args) {
+		int a[] = {1,10,3,5,77,7};
+		Arrays.sort(a);
+		System.out.println(a);
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.binarySearch(a, -1));
+		System.out.println(Arrays.binarySearch(a, 2));
+		System.out.println(Arrays.binarySearch(a, 4));
+		System.out.println(Arrays.binarySearch(a, 8));
+		System.out.println(Arrays.binarySearch(a, 11));
+		System.out.println(Arrays.binarySearch(a, 100));
+	}
+
+}
